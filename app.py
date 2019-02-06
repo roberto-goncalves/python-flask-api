19332acb135125b820283d290f80b53c881ec306
Roberto Goncalves Santos Junior <roberto.forsaken@gmail.com>
2019-02-06 14:13:30 -0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Flask-API Dockerized on port 80'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
