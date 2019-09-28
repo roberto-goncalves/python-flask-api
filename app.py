d0691701db9088a93a8eb98571196c736ba0ec15
Roberto Gonçalves <roberto.forsaken@gmail.com>
2019-09-28 15:09:23 -0300

    Putting return on route

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-From flask import Flask
+from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+@app.route('/', methods=['POST'])
+def return_post():
+    print request.data
+    return request.data
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=9095)
