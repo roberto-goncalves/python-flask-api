1bcd34dbc12d7e7c9ab487f7f4b3cafe906cc3d4
Roberto Goncalves <roberto.santos@luizalabs.com>
2021-05-18 13:49:23 -0300

    dronecd

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['POST'])
 def return_post():
-    print testpush
     print request.data
     return request.data
 
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=9095)
+    app.run(debug=True, host='0.0.0.0', port=9095)
