dead636ec9f3a6a2452ed87fb72d9855d482062b
Roberto Goncalves <roberto.santos@luizalabs.com>
2021-05-18 14:30:08 -0300

    dronecd

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def return_post():
     """ DOCSTRING """
-
-
     print(request.data)
     return request.data
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9095)
