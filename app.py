13a4a95d51c14930fa10df68c19d420380ca0328
Roberto Goncalves <roberto.santos@luizalabs.com>
2021-05-28 13:33:06 -0300

    printing test

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
 """ DOCSTRING """
 
-from flask import Flask
-from flask import request
-
-app = Flask(__name__)
-
-
-@app.route('/', methods=['POST'])
-def return_post():
-    """ DOCSTRING """
-    print(request.data)
-    return request.data
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=9095)
+print(test)
